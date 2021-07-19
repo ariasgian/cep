6a4dd1f70ae6f9dcbf25767c4c28a7c75fac5aaa
ariasgian <ariasgian@gmail.com>
2021-07-18 22:50:14 -0300

    se agrega seleccion del terminal en el formulario

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ Editor de Spyder
 
 Este es un archivo temporal.
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import Form, validators, RadioField, SelectField, SubmitField,StringField
+from wtforms.validators import DataRequired
 import cargar_datos
-
-
 import pandas as pd
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mykey'
+
+class InfoForm(FlaskForm):
+    terminaL_cpk = RadioField('r', choices=[])
+    Submit = SubmitField('Seleccionar')
+    
 
 #%%
 
@@ -23,16 +31,31 @@ def images():
     
     #data = pd.DataFrame(matriz)
     tamano= len(data)
-    return render_template('test.html',tables= data, tam=tamano)
+    form = InfoForm()
+    if request.method =='GET':
+        return render_template('test.html',tables= data, tam=tamano, form= form)
+    if request.method =='POST':
+        abc= request.form.get('terminal')
+        print(abc)
+        print('OK')
+        return abc
     #return render_template('test.html', graphJSON=graphJSON)
      
 @app.route('/test')   
-
 def test():
     matriz= {'terminal': ['abc', 'cde'], 'cpk': [1, 2]}
     data = pd.DataFrame(matriz)
     for item in data:
         print(item)
     return 'observa la consola'
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    form = InfoForm()
+    if form.validate_on_submit():
+        print('ok')
+    
+
+
 if __name__ == '__main__':
     app.run(port='8050', host='0.0.0.0',debug=True)
